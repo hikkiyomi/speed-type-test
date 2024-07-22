70bf9bb4e10cd864e06f7071ec592c0c0b763405
Mikhail Mefodyev <hg-4ybak@mail.ru>
2024-07-22 21:20:26 +0300

    feat: init commit

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
+
+const (
+	DEBUG_FILE   = "debug.log"
+	DEBUG_PREFIX = "DEBUG"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "stt",
+	Short: "Type stt to start the application.",
+	Run: func(cmd *cobra.Command, args []string) {
+		f, err := tea.LogToFile(DEBUG_FILE, DEBUG_PREFIX)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		if _, err := tea.NewProgram().Run(); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hikkiyomi/speed-type-test/cmd"
+
+func main() {
+	cmd.Execute()
+}
